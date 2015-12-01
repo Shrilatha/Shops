30768dc43271b25c6d5cf3b78f6a1bf66aa62c17
Shrilatha <shrilatha@Axaratech-HP-430-Notebook-PC.(none)>
2015-12-01 09:39:55 +0530

    Second Commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Shops::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'shop#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
